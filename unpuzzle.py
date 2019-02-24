45bdbcc3978536127208667926ad1eb76b81d3f7
gomamon <leeye9779@gmail.com>
2019-02-24 18:27:37 +0900

    Using laplacian to improve accurcy

--- unpuzzle.py
+++ unpuzzle.py
@@ -6,70 +6,30 @@ import piece
 
 
 
-def get_color(img, row, col, flag):
-    # <flag::: 0:top, 1:right, 2:bottom, 3:left, 4:all>
-
-    if flag == 0:
-        return [
-            list(img[0, 0]),
-            list(img[1, 0]),
-            list(img[0, int(col / 3)]),
-            list(img[1, int(col / 3)]),
-            list(img[0, col-1]),
-            list(img[1, col-1])
-        ]
-
-    if flag == 2:
-        return [
-            list(img[row - 1, 0]),
-            list(img[row - 2, 0]),
-            list(img[row - 1, int(col / 3)]),
-            list(img[row - 2, int(col / 3)]),
-            list(img[row - 1, col-1]),
-            list(img[row - 2, col-1])
-        ]
-
-    if flag == 4:
-        return [
-            img[0, 0],
-            img[0, int(col / 2)],
-            img[0, col - 1],
-            img[int(row / 2), col - 1],
-            img[row - 1, col - 1],
-            img[row - 1, int(col / 2)],
-            img[row - 1, 0],
-            img[int(row / 2), 0]
-    ]
-
-def get_color_diff(dst, src):
-    diff = 0
-    tot_diff = 0
-    for i in range(6):
-        for j in range(3):
-            diff += abs(int(dst[i][j]) - int(src[i][j]))
-        diff /= 3
-    diff /= 3
-    return diff
-
-
-
-def get_horiz_line_score(img, n, piece_row, piece_col):
+def get_line_score(img, n, piece_row, piece_col, flag):
 
     print(piece_row*n)
     score = 0
     print(img)
-    for i in range(piece_col):
-        if img[piece_row*n, i] > 30:
-            score += 1
-        if img[piece_row*n-1, i] > 30:
-            score+=1
-        if img[piece_row*n+1, i] > 30:
-            score += 1
 
+    if flag == 0:
+        for i in range(piece_col):
+            if img[piece_row*n, i] > 30:
+                score += 1
+            if img[piece_row*n-1, i] > 30:
+                score+=1
+            if img[piece_row*n+1, i] > 30:
+                score += 1
+    else:
+        for i in range(piece_row):
+            if img[i, piece_col*n] > 30:
+                score += 1
+            if img[i, piece_col*n-1] > 30:
+                score+=1
+            if img[i, piece_col*n+1] > 30:
+                score += 1
 
     return score
-
-
 
 
 
@@ -84,8 +44,6 @@ def vertical_merge(pieces, remainder, row, col, p, q):
     remainder.pop(0)
     num_merged = 1
     merge_info = []
-    line_score = -1
-    bottom_flag = 0
 
 
     while num_merged < p:
@@ -102,7 +60,7 @@ def vertical_merge(pieces, remainder, row, col, p, q):
                     gray_src = cv2.cvtColor(pieces[src_idx].img, cv2.COLOR_BGR2GRAY)
                     tmp_merged = cv2.vconcat([gray_dst, gray_src])
                     laplacian = cv2.Laplacian(tmp_merged, cv2.CV_8U, ksize=3)
-                    line_score = get_horiz_line_score(laplacian, num_merged, piece_row, piece_col)
+                    line_score = get_line_score(laplacian, num_merged, piece_row, piece_col,0)
 
                     if min_line_score == -1 or min_line_score > line_score:
                         merge_info = [flip_flag_dst, flip_flag_src, src_idx]
@@ -121,6 +79,49 @@ def vertical_merge(pieces, remainder, row, col, p, q):
     return merged
 
 
+def horizontal_merge(pieces, remainder, row, col, p, q):
+    piece_row = row
+    piece_col = int(col / q)
+
+    merged = pieces[remainder[0]].copy()
+    merged = pieces[remainder[0]][:piece_row, :piece_col]
+
+    remainder.pop(0)
+    num_merged = 1
+    merge_info = []
+
+
+    while num_merged < p:
+        # Get minimum diff and save information
+        min_line_score = -1
+
+        for src_idx in remainder:
+            for flip_flag_dst in range(4):
+                merged = cv2.flip(merged, flip_flag_dst % 2)
+                gray_dst = cv2.cvtColor(merged, cv2.COLOR_BGR2GRAY)
+                for flip_flag_src in range(4):
+                    pieces[src_idx] = cv2.flip(pieces[src_idx], flip_flag_src % 2)
+
+                    gray_src = cv2.cvtColor(pieces[src_idx], cv2.COLOR_BGR2GRAY)
+                    tmp_merged = cv2.hconcat([gray_dst, gray_src])
+                    laplacian = cv2.Laplacian(tmp_merged, cv2.CV_8U, ksize=3)
+                    line_score = get_line_score(laplacian, num_merged, piece_row, piece_col, 1)
+
+                    if min_line_score == -1 or min_line_score > line_score:
+                        merge_info = [flip_flag_dst, flip_flag_src, src_idx]
+                        min_line_score = line_score
+
+        if num_merged < p:
+            for flip_flag_dst in range(merge_info[0] + 1):
+                merged = cv2.flip(merged, flip_flag_dst % 2)
+            for flip_flag_src in range(merge_info[1] + 1):
+                pieces[merge_info[2]] = cv2.flip(pieces[merge_info[2]], flip_flag_src % 2)
+            merged = cv2.hconcat([merged, pieces[merge_info[2]]])
+
+            remainder.pop(remainder.index(merge_info[2]))
+            num_merged += 1
+
+    return merged
 
 
 if __name__ == '__main__':
@@ -137,22 +138,22 @@ if __name__ == '__main__':
     piece_col = int(col/q)
     pieces = []
 
-    #match column images
+    #match vertical images
     for i in range(p):
         for j in range(q):
             pieces.append(piece.Piece(piece_row, piece_col,
                      puzzled_img[i*piece_row:i*piece_row+piece_row, j*piece_col:j*piece_col+piece_col]))
 
     remainder = [i for i in range(p*q)]
-
+    vertical_pieces = []
     for i in range(q):
-        cv2.imshow("laplacian", vertical_merge(pieces, remainder, row, col, p, q))
+        vertical_pieces.append(vertical_merge(pieces, remainder, row, col, p, q))
+        cv2.imshow("laplacian", vertical_pieces[0])
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-
-    #cv2.imshow("completed!", match_row(completed_col, unchecked, row, col, p, q))
-
+    remainder = [i for i in range(q)]
+    cv2.imshow("completed!", horizontal_merge(vertical_pieces, remainder, row, col, p, q))
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
